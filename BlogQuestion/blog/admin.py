fad83251b46fe4c2ae987d76d19e88514eb6486d
mashmool0 <ap4games2017@gmail.com>
2024-05-06 01:13:26 +0330

    Solve Blog question fixed

--- BlogQuestion/blog/admin.py
+++ BlogQuestion/blog/admin.py
@@ -1,1 +1,7 @@
 from django.contrib import admin
+from .models import Author, BlogPost, Comment
+
+# Register your models here.
+admin.site.register(Author)
+admin.site.register(BlogPost)
+admin.site.register(Comment)
